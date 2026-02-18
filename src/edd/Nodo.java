b1b3a778e53fc37281bf95154ce37caf7d1808c4
Jesusrod06 <144686319+Jesusrod06@users.noreply.github.com>
2026-02-18 19:41:22 -0400

    Introducimos clase Nodo en el package edd

---
+++ src/edd/Nodo.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edd;
+
+/**
+ *
+ * @author jesus rodriguez
+ */
+public class Nodo <T> {
+    private T contenido;
+    
+    private Nodo<T> siguiente;
+
+    public Nodo(T contenido) {
+        this.contenido = contenido;
+        this.siguiente = null;
+    }
+
+    /**
+     * @return the contenido
+     */
+    public T getContenido() {
+        return contenido;
+    }
+
+    /**
+     * @param contenido the contenido to set
+     */
+    public void setContenido(T contenido) {
+        this.contenido = contenido;
+    }
+
+    /**
+     * @return the siguiente
+     */
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    /**
+     * @param siguiente the siguiente to set
+     */
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
