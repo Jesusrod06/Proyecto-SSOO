b54c7ee09d16ca522b02d62fca72d6d25e09a51c
Jesusrod06 <144686319+Jesusrod06@users.noreply.github.com>
2026-02-19 14:28:36 -0400

    restauracion clases de edd

--- src/edd/Cola.java
+++ src/edd/Cola.java
@@ -8,6 +8,89 @@ package edd;
  *
  * @author jesus rodriguez
  */
-public class Cola {
+public class Cola <T>{
+    private Nodo<T> cabeza;
+    private Nodo<T> cola;
+    private int tamano;
+
+    public Cola() {
+        this.cabeza = null;
+        this.cola = null;
+        this.tamano = 0;
+    }
+
+    public boolean esVacia() {
+        return cabeza == null;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    // 1. ENCOLAR: Inserta al final
+    public void encolar(T dato) {
+        Nodo<T> nuevoNodo = new Nodo<>(dato);
+        if (esVacia()) {
+            cabeza = nuevoNodo;
+            cola = nuevoNodo;
+        } else {
+            this.cola.setSiguiente(nuevoNodo);
+            this.cola = nuevoNodo;
+        }
+        tamano++;
+    }
+
+    // 2. DESENCOLAR: Saca por el frente (Para dárselo a la CPU)
+    public T desencolar() {
+        if (esVacia()) return null;
+        
+        T dato = cabeza.getContenido();
+        cabeza = cabeza.getSiguiente();
+        
+        if (cabeza == null) {
+            cola = null;
+        }
+        tamano--;
+        return dato;
+    }
+
+    // 3. OBTENER: Para que la Interfaz Gráfica pueda pintar la cola
+    public T obtener(int indice) {
+        if (indice < 0 || indice >= tamano) return null;
+        Nodo<T> actual = cabeza;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.getSiguiente();
+        }
+        return actual.getContenido();
+    }
+
+    // 4. ELIMINAR ESPECÍFICO: Para sacar procesos y mandarlos a Suspendido (Swapping)
+    public boolean eliminar(T dato) {
+        if (esVacia()) return false;
+
+        if (cabeza.getContenido().equals(dato)) {
+            desencolar();
+            return true;
+        }
+
+        Nodo<T> actual = cabeza;
+        while (actual.getSiguiente() != null) {
+            if (actual.getSiguiente().getContenido().equals(dato)) {
+                actual.setSiguiente(actual.getSiguiente().getSiguiente());
+                if (actual.getSiguiente() == null) {
+                    cola = actual;
+                }
+                tamano--;
+                return true;
+            }
+            actual = actual.getSiguiente();
+        }
+        return false;
+    }
     
+    public void vaciar() {
+        this.cabeza = null;
+        this.cola = null;
+        this.tamano = 0;
+    }
 }
--- src/edd/Lista.java
+++ src/edd/Lista.java
@@ -8,6 +8,80 @@ package edd;
  *
  * @author jesus rodriguez
  */
-public class Lista {
-    
+public class Lista <T> {
+private Nodo<T> cabeza;
+    private int tamano;
+
+    // Constructor
+    public Lista() {
+    this.cabeza = null;
+        this.tamano = 0;
+    }
+
+    public boolean esVacia() {
+        return cabeza == null;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    // 1. INSERTAR: Agrega un elemento al final de la lista
+    public void insertar(T dato) {
+        Nodo<T> nuevoNodo = new Nodo<>(dato);
+        if (esVacia()) {
+            cabeza = nuevoNodo;
+        } else {
+            Nodo<T> aux = cabeza;
+            while (aux.getSiguiente() != null) {
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(nuevoNodo);
+        }
+        tamano++;
+    }
+
+    // 2. OBTENER: Devuelve el elemento en una posición específica
+    public T obtener(int indice) {
+        if (indice < 0 || indice >= tamano) {
+            return null;
+        }
+        Nodo<T> aux = cabeza;
+        for (int i = 0; i < indice; i++) {
+            aux = aux.getSiguiente();
+        }
+        return aux.getContenido();
+    }
+
+    // 3. ELIMINAR: Busca un elemento y lo borra (Útil para liberar RAM)
+    public boolean eliminar(T dato) {
+        if (esVacia()) {
+            return false;
+        }
+
+        // Si es el primero
+        if (cabeza.getContenido().equals(dato)) {
+            cabeza = cabeza.getSiguiente();
+            tamano--;
+            return true;
+        }
+
+        // Si está en el medio o al final
+        Nodo<T> aux = cabeza;
+        while (aux.getSiguiente() != null) {
+            if (aux.getSiguiente().getContenido().equals(dato)) {
+                aux.setSiguiente(aux.getSiguiente().getSiguiente());
+                tamano--;
+                return true;
+            }
+            aux = aux.getSiguiente();
+        }
+        return false;
+    }
+
+    //  VACIAR: Limpia la lista completa
+    public void vaciar() {
+        cabeza = null;
+        tamano = 0;
+    }
 }
