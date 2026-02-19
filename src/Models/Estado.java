fa55ccc2892c81f50f2af35522d041e6dcc20176
JesusLealC <jesus.leal@correo.unimet.edu.ve>
2026-02-19 12:52:12 -0400

    Update Estado.java

--- src/Models/Estado.java
+++ src/Models/Estado.java
@@ -1,13 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ */  
+
+
 package Models;
 
-/**
- *
- * @author jesus rodriguez
- */
-public class Estado {
-    
+public enum Estado {
+    NUEVO,
+    LISTO,
+    EJECUCION,
+    BLOQUEADO,
+    TERMINADO,
+    LISTO_SUSPENDIDO,
+    BLOQUEADO_SUSPENDIDO
 }
